b4019cf2b7ed9c02d48d53eccd08dd93f115000a
yogesh832 <upadhayayyogesh832@gmail.com>
2024-06-12 21:42:19 +0500

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,21 @@ const scroll = new LocomotiveScroll({
 
 
 
+function toggleMenu() {
+    var navbar = document.getElementById('navbar');
+    navbar.classList.toggle('active');
+}
+
+function openModal() {
+    var modal = document.getElementById('modal');
+    modal.style.display = 'block';
+}
+
+function closeModal() {
+    var modal = document.getElementById('modal');
+    modal.style.display = 'none';
+}
+
 
 function openModal() {
     document.getElementsByClassName("modal-signIn")[0].style.display = "block";
@@ -19,11 +34,12 @@ function closeModal() {
 
 // Close the modal when clicking outside of it
 window.addEventListener("click", function(event) {
-    const modal = document.querySelector("modal-signIn")[0];
+    const modal = document.querySelector(".modal-signIn");
     if (event.target === modal) {
         closeModal();
     }
 });
+// to toggle the footer
 
 function openMaterial(){
     document.getElementsByClassName("material")[0].style.display = "block";
@@ -37,6 +53,7 @@ function closeMaterial(){
 }
 
 
+
 function openMaterial1(){
     document.getElementsByClassName("material")[1].style.display = "block";
     document.getElementsByClassName("add")[1].style.display = "none";
@@ -47,3 +64,16 @@ function closeMaterial1(){
     document.getElementsByClassName("add")[1].style.display = "block";
     document.getElementsByClassName("remove")[1].style.display = "none";
 }
+
+
+
+
+
+
+//responsiveness
+function toggleMenu(){
+    var navbar = document.getElementsById('navbar');
+    navbar.classList.toggle("active");
+}
+
+
