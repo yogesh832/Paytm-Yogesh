dc3ca01cec1bed87b0e4e87f95e3a7059ca6931e
yogesh832 <upadhayayyogesh832@gmail.com>
2024-06-16 08:56:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 
 
-document.addEventListener("DOMContentLoaded",()=>{
 
-    // this is to smooth scrolling
+  // this is to smooth scrolling
 const scroll = new LocomotiveScroll({
-    el: document.querySelector(".all-content"),
-    smooth: true
+  el: document.querySelector(".all-content"),
+  smooth: true
 });
 
 
@@ -16,18 +15,18 @@ var navbar = document.getElementById("navbar");
 var hamburgerClose=document.getElementById("hamburger-close");
 
 function closeHamburgerMenu(){
-   menuIcon.style.display="none";
-    hamburgerClose.style.display="block";
+ menuIcon.style.display="none";
+  hamburgerClose.style.display="block";
 hamburgerClose.addEventListener("click",()=>{
-    navbar.classList.toggle("active");
-    hamburgerClose.style.display="none";
-    menuIcon.style.display="block";
+  navbar.classList.toggle("active");
+  hamburgerClose.style.display="none";
+  menuIcon.style.display="block";
 });
 }
 
 menuIcon.addEventListener("click", () => {
-     navbar.classList.toggle("active");
-     closeHamburgerMenu();
+   navbar.classList.toggle("active");
+   closeHamburgerMenu();
 });
 
 
@@ -37,57 +36,43 @@ menuIcon.addEventListener("click", () => {
 var modal = document.getElementById("modal");
 
 function openModal() {
-    modal.style.display = "block";
+  modal.style.display = "block";
 }
 
 function closeModal() {
-    modal.style.display = "none";
+  modal.style.display = "none";
 }
 
 document.querySelector(".signin").addEventListener("click", openModal);
 document.querySelectorAll(".close-button").forEach(button => {
-    button.addEventListener("click", closeModal);
+  button.addEventListener("click", closeModal);
 });
 
 document.addEventListener("click", (event) => {
-    if (!modal.contains(event.target) && !event.target.closest(".signin")) {
-        closeModal();
-    }
-});
-
-
-// to toggle the footer
-
-function openMaterial(){
-    document.getElementsByClassName("material")[0].style.display = "block";
-    document.getElementsByClassName("add")[0].style.display = "none";
-    document.getElementsByClassName("remove")[0].style.display = "block";
-}
-function closeMaterial(){
-    document.getElementsByClassName("material")[0].style.display = "none";
-    document.getElementsByClassName("add")[0].style.display = "block";
-    document.getElementsByClassName("remove")[0].style.display = "none";
-}
-
-
-
-function openMaterial1(){
-    document.getElementsByClassName("material")[1].style.display = "block";
-    document.getElementsByClassName("add")[1].style.display = "none";
-    document.getElementsByClassName("remove")[1].style.display = "block";
-}
-function closeMaterial1(){
-    document.getElementsByClassName("material")[1].style.display = "none";
-    document.getElementsByClassName("add")[1].style.display = "block";
-    document.getElementsByClassName("remove")[1].style.display = "none";
-}
-
-
-
-
-
+  if (!modal.contains(event.target) && !event.target.closest(".signin")) {
+      closeModal();
+  }
 });
 
 
 
+
+
+
+
+
+// to toggle the footer's material
+function openMaterial(id) {
+  const section = document.getElementById(id);
+  section.querySelector(".material").style.display = "block";
+  section.querySelector(".add").style.display = "none";
+  section.querySelector(".remove").style.display = "block";
+}
+
+function closeMaterial(id) {
+  const section = document.getElementById(id);
+  section.querySelector(".material").style.display = "none";
+  section.querySelector(".add").style.display = "block";
+  section.querySelector(".remove").style.display = "none";
+}
 
